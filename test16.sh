dbf2764f1e0655bc12f82e3b228121ae81011441
xavyaly <wellboy.alam13@gmail.com>
2018-08-29 08:27:38 +0530

    added 16 & 17

---
+++ test16.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "I am calling with $# parameters"
+echo "My script name is $0"
+echo "My first parameter is $1"
+echo "My second parameter is $2"
+echo "My third parameter is $3"
+echo "All parameters is $@"
